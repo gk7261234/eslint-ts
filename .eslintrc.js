2cfd2060ef9aa12dc3eff0f6ac9edc269c8f4c03
gk7261234 <1011464909@qq.com>
2022-03-14 15:25:20 +0800

    feat: pre-commit优化

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    react: {
+      version: '999.999.999'
+    }
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-console': 0
+  }
+}
